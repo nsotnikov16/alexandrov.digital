82c5e3c9f3a92e2c3673fde7f1598020fd4c537e
Nikita Sotnikov <nsotnikov16@yandex.ru>
2024-09-18 09:56:06 +0500

    update

---
+++ dev/template/views/note.php
@@ -0,0 +1,8 @@
+<div class="note">
+    <div class="note__icon">
+        <span class="note__svg" data-img-svg data-src="<?= ASSETS_PATH ?>images/icons/note.svg"></span>
+    </div>
+    <div class="note__text">
+        <?= $text ?>
+    </div>
+</div>
